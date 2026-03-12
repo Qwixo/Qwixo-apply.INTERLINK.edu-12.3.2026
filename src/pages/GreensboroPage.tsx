68d71d675d23b0dc911f75d792a0581e0e1b51aa
Qwixo <101059562+Qwixo@users.noreply.github.com>
2026-03-12 17:59:19 +0100

    Updated GreensboroPage.tsx

--- src/pages/GreensboroPage.tsx
+++ src/pages/GreensboroPage.tsx
@@ -238,7 +238,7 @@ function GreensboroPage() {
                   <div className="space-y-2">
             
                   </div>
-                  <p className="font-semibold mt-3">
+                  <p className="font-semibold mt-4">
                     You've probably wondered: What am I doing wrong?
                   </p>
                 </div>
